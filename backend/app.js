1b065b1bfd75aa093889dffac24a0d46298b13e6
weizmanfabian <weizmanfabian@gmail.com>
2022-04-21 16:49:47 -0500

    upload and delete images

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ import express from 'express';
 import cors from "cors";
 import db from "./database/db.js";
 import blogRoutes from "./routes/routes.js"
+import imgRoutes from "./routes/imgRoutes.js"
+import { port } from './config.js';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(_dirname, './public')))
+
 app.use('/blogs', blogRoutes)
+app.use('/upload', imgRoutes)
 
 try {
   await db.authenticate()
@@ -16,8 +22,12 @@ try {
   console.log(`error en la conexión ${err}`);
 }
 
+
+
 app.get('/', (req, res) => {
   res.send('Estás en el servidor')
 })
 
-app.listen(8000, () => console.log(`server UP running in http://localhost:8000`))
+
+
+app.listen(port, () => console.log(`server UP running in http://localhost:${port}`))
